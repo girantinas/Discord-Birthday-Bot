1f5bc10a3a004a22b612a2efe0ba19076b7b67e3
girantinas <girantinas@github.com>
2020-10-20 12:27:24 -0400

    tidying up

--- bdaybot.py
+++ bdaybot.py
@@ -1,120 +1,11 @@
 import os
 import json
 import discord
-from dotenv import load_dotenv
-from discord.ext import commands, tasks
 import time, datetime
 import asyncio
-
-### Helper Functions ###
-
-_MONTHNAMES = [None, 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
-                    'September', 'October', 'November', 'December']
-
-def posixtime_to_str(posix_time):
-    """Takes a posixtime and returns its representation in the following format:
-    <Month Name> <Day> <Ordinal Suffix>, <Optional Year>. 
-    Year 0 does not exist so it is used to indicate a date with no definite year.
-    """
-    dt = datetime.datetime.utcfromtimestamp(posix_time)
-    suffix = 'th'
-    if dt.day % 10 == 1:
-        suffix = 'st'
-    elif dt.day % 10 == 2:
-        suffix = 'nd'
-    elif dt.day % 10 == 3:
-        suffix = 'rd'
-
-    date_string = f"{_MONTHNAMES[dt.month]} {dt.day}{suffix}"
-
-    # Only nonzero years are displayed
-    if dt.year:
-        date_string = f"{date_string}, {dt.year}"
-    
-    return date_string
-
-# An implementation of current DST (Daylight Savings Time) rules for major US time zones 2007 and later.
-# Adapted from: https://docs.python.org/3/library/datetime.html#datetime.tzinfo
-
-ZERO = datetime.timedelta(0)
-HOUR = datetime.timedelta(hours=1)
-SECOND = datetime.timedelta(seconds=1)
-
-def first_sunday_on_or_after(dt):
-    """Finds the first sunday on or after a specific datetime.
-    """
-    days_to_go = 6 - dt.weekday()
-    if days_to_go:
-        dt += datetime.timedelta(days_to_go)
-    return dt
-
-def us_dst_range(year):
-    """ Returns start and end times for US DST on a given year after 2006.
-    ---------------------------------------------------------------------
-    US DST Rules
-
-    This is a simplified (i.e., wrong for a few cases) set of rules for US
-    DST start and end times. For a complete and up-to-date set of DST rules
-    and timezone definitions, visit the Olson Database (or try pytz):
-    http://www.twinsun.com/tz/tz-link.htm
-    http://sourceforge.net/projects/pytz/ (might not be up-to-date)
-
-    In the US, since 2007, DST starts at 2am (standard time) on the second
-    Sunday in March, which is the first Sunday on or after Mar 8.
-    and ends at 2am (DST time) on the first Sunday of Nov.
-    """
-    start = first_sunday_on_or_after(datetime.datetime(1, 3, 8, 2).replace(year=year))
-    end = first_sunday_on_or_after(datetime.datetime(1, 11, 1, 2).replace(year=year))
-    return start, end
-
-class USTimeZone(datetime.tzinfo):
-    """A Class representing a USTimeZone, complete with DST. Inherits from datetime.tzinfo.
-    """
-
-    def __init__(self, hours):
-        self.stdoffset = datetime.timedelta(hours=hours)
-
-    def utcoffset(self, dt):
-        return self.stdoffset + self.dst(dt)
-
-    def dst(self, dt):
-        """Calculates the timezone offset due to DST. Overriden from the tzinfo interface.
-        """
-        assert dt is not None and dt.tzinfo is self, "Invalid Datetime Object"
-        start, end = us_dst_range(dt.year)
-        # Can't compare naive to aware objects, so strip the timezone from
-        # dt first.
-        dt = dt.replace(tzinfo=None)
-        if start + HOUR <= dt < end - HOUR:
-            # DST is in effect.
-            return HOUR
-        if end - HOUR <= dt < end:
-            # Fold (an ambiguous hour): use dt.fold to disambiguate.
-            return ZERO if dt.fold else HOUR
-        if start <= dt < start + HOUR:
-            # Gap (a non-existent hour): reverse the fold rule.
-            return HOUR if dt.fold else ZERO
-        # DST is off.
-        return ZERO
-
-    def fromutc(self, dt):
-        """Overriding the fromutc function in tzinfo.
-        """
-        assert dt.tzinfo is self
-        start, end = us_dst_range(dt.year)
-        start = start.replace(tzinfo=self)
-        end = end.replace(tzinfo=self)
-        std_time = dt + self.stdoffset
-        dst_time = std_time + HOUR
-        if end <= dst_time < end + HOUR:
-            # Repeated hour
-            return std_time.replace(fold=1)
-        if std_time < start or dst_time >= end:
-            # Standard time
-            return std_time
-        if start <= std_time < end - HOUR:
-            # Daylight savings time
-            return dst_time
+from dotenv import load_dotenv
+from discord.ext import commands, tasks
+from bdayhelpers import posixtime_to_str, USTimeZone
 
 ### Environment Setup ###
 
@@ -246,7 +137,7 @@ async def celebrate_birthdays(serverid):
         birthday_dt = datetime.datetime.fromtimestamp(birthdays[userid]['bday'], tz=datetime.timezone.utc)
         if birthday_dt.month == now_dt.month and birthday_dt.day == now_dt.day:
             print(f"{userid}'s bday was celebrated")
-            await channel.send(f'🎊 Happy Birthday <@{userid}> 🎊')
+            await channel.send(f'🎊 Happy Birthday <@{userid}>!!!!!!!!!!!!!! 🎊')
 
 @tasks.loop(hours=24)
 async def celebrate_all_server_birthdays():
@@ -262,11 +153,11 @@ async def celebrate_all_server_birthdays():
 async def before():
     await bot.wait_until_ready()
     now_dt = datetime.datetime.now(tz=TIMEZONE)
-    print("Initialized at{0:2d}:{1:2d}:{2:2d} on {3}/{4}/{5} (In the set Timezone)".format(now_dt.hour, now_dt.minute, now_dt.second, 
+    print("Initialized at {0:0=2d}:{1:1=2d}:{2:2=2d} on {3}/{4}/{5} (In the set Timezone)".format(now_dt.hour, now_dt.minute, now_dt.second, 
                                                                                             now_dt.month, now_dt.day, now_dt.year))
     # Want to execute at 12:01 AM the next day.
     midnight_dt = (now_dt + datetime.timedelta(days=1)).replace(hour=0, minute=1, second=0)
-    #await asyncio.sleep(midnight_dt.timestamp() - time.time())
+    await asyncio.sleep(midnight_dt.timestamp() - time.time())
 
 celebrate_all_server_birthdays.start()        
 bot.run(TOKEN)
